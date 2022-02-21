f67eab0275423c1d2f9f4e85039f9346e3e68073
Morgan <cfzhangwl@163.com>
2022-02-21 15:53:50 +0800

    2.21 提交

--- Test/Rectangle.cs
+++ Test/Rectangle.cs
@@ -8,5 +8,26 @@ namespace Test
 {
     public class Rectangle
     {
+        double length;
+        double width;
+
+        public void Acceptdetails()
+        {
+            length = 4.5;
+            width = 3.5;
+        }
+
+        public double GetArea()
+        {
+            return length * width;
+        }
+
+        public void Display()
+        {
+            Console.WriteLine("Length:{0}", length);
+            Console.WriteLine("Width:{0}", width);
+            Console.WriteLine("Area:{0}", GetArea());
+        } 
+
     }
 }
--- Test/SampleClass.cs
+++ Test/SampleClass.cs
@@ -8,5 +8,15 @@ namespace Test
 {
     public class SampleClass
     {
+        public int x;
+        public int y;
+        public const int c1 = 5;
+        public const int c2 = c1 + 5;
+
+        public SampleClass(int p1,int p2)
+        {
+            x = p1;
+            y = p2;
+        } 
     }
 }
