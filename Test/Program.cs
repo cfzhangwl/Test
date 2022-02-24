20a9a790ae06b96ccffbad733d10a2549a1cd047
Morgan <cfzhangwl@163.com>
2022-02-24 15:41:57 +0800

    init

--- Test/Program.cs
+++ Test/Program.cs
@@ -6,59 +6,7 @@ namespace Test
     {
         static void Main(string[] args)
         {
-            #region 隱藏
-
-            /*
-            Console.WriteLine("Hello World!");
-            Console.ReadKey();
-            */
-
-            /*  Rectangle r = new Rectangle();
-              r.Acceptdetails();
-              r.Display();
-             */
-
-            //Console.WriteLine("Size of int :{0}", sizeof(int));
-
-            /*
-            short a;
-            int b;
-            double c;
-            a = 10;
-            b = 20;
-            c = a + b;
-            Console.WriteLine("a={0},b={1},c={2}",a,b,c);
-            Console.ReadLine();
-            */
-
-            /*
-            int num;
-            num = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine(num);
-            */
-
-            //Console.WriteLine("Hello\tWorld\n\n");
-
-            /*   
-             SampleClass mC = new SampleClass(11,22);
-             Console.WriteLine("x={0},y={1}",mC.x,mC.y);
-             Console.WriteLine("c1={0},c2={1}",SampleClass.c1,SampleClass.c2);
-            */
-
-            /*
-            Console.WriteLine("int 的大小是{0}",sizeof(int));
-            Console.WriteLine("short 的大小是{0}",sizeof(short));
-            Console.WriteLine("double 的大小是{0}",sizeof(double));
-
-            int a, b;
-            a = 10;
-            b = (a == 1) ? 20 : 30;
-            Console.WriteLine("b的值是{0}",b);
-
-            b = (a == 10) ? 20 : 30;
-            Console.WriteLine("b的值是{0}", b);
-            */
-            #endregion
+             
         }
     }
 }
